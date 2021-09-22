526ff755703a14ac9baf5ac5180c3d9c7644be83
PoeticPythonist <86872809+PoeticPythonist@users.noreply.github.com>
2021-09-22 12:50:11 +0300

    Add files via upload

---
+++ test/a_file.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import plotly.graph_objects as go
+import numpy as np
+import plotly.express as px
+
+# Helix equation
+t = np.linspace(0, 20, 100)
+x, y, z = np.cos(t), np.sin(t), t
+
+fig = go.Figure(data=[go.Scatter3d(
+    x=x,
+    y=y,
+    z=z,
+    mode='markers',
+    marker=dict(
+        size=12,
+        color=z,                # set color to an array/list of desired values
+        colorscale='Viridis',   # choose a colorscale
+        opacity=0.8
+    )
+)])
+
+# tight layout
+fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+fig.write_html("test.html") #Modifiy the html file
+fig.show()
